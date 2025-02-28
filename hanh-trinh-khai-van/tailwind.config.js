217cdc60d26fbc58df08ad9178fe8e7372a7355c
unknown <ocoten321@gmail.com>
2025-02-28 12:27:23 +0700

    first commit

---
+++ hanh-trinh-khai-van/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '0rem',
+    },
+    screens: {
+      'xl': '1355px',
+      '2xl': '1425px',
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    }
+  },
+  plugins: [],
+}
